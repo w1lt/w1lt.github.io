3ce9a429596fc381c7e4bb4dc214c89123700e38
William Whitehead <willclashtest@gmail.com>
2023-09-12 17:06:57 -0500

    added persistent dark mode

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
+/** @format */
+
 function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+  localStorage.setItem("dark-mode", this.checked);
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+if (localStorage.getItem("dark-mode") === "true") {
+  body.classList.add("dark-mode");
 }
