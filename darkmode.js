ce1027d91c09bafcb60dfb207bb1fd9c0a32b2a6
William Whitehead <willclashtest@gmail.com>
2023-09-12 21:28:22 -0500

    fixed persistant dark mode

--- darkmode.js
+++ darkmode.js
@@ -1,20 +1,26 @@
 /** @format */
+
+const emoji = document.getElementById("emoji");
+
 function darkMode() {
   localStorage.setItem("dark-mode", true);
-  console.log("dark mode");
   if (document.body.classList.contains("dark-mode")) {
     document.body.classList.remove("dark-mode");
+    emoji.innerHTML = "🌞";
     localStorage.setItem("dark-mode", false);
   } else {
     document.body.classList.add("dark-mode");
+    emoji.innerHTML = "🌚";
+    console.log("dark mode");
   }
 }
 
 if (localStorage.getItem("dark-mode")) {
-  console.log("read from memory");
   if (localStorage.getItem("dark-mode") === "true") {
     document.body.classList.add("dark-mode");
+    emoji.innerHTML = "🌚";
   } else {
     document.body.classList.remove("dark-mode");
+    emoji.innerHTML = "🌞";
   }
 }
