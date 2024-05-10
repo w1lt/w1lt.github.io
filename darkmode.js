9b27a879f87e70f629e09655e17fa104e2789061
William Whitehead <willclashtest@gmail.com>
2024-05-10 15:19:11 -0500

    style: Refactor dark mode toggle in resume.html and darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -3,22 +3,27 @@
 const emoji = document.getElementById("emoji");
 function darkMode() {
   if (document.body.classList.contains("dark-mode")) {
-    document.body.classList.remove("dark-mode");
-    emoji.innerHTML = "☀";
+    enableDarkMode();
     localStorage.setItem("dark-mode", "false");
   } else {
-    document.body.classList.add("dark-mode");
-    emoji.innerHTML = "☾";
+    disableDarkMode();
     localStorage.setItem("dark-mode", "true");
-    console.log("dark mode");
   }
 }
 
 if (localStorage.getItem("dark-mode") === "true") {
-  document.body.classList.add("dark-mode");
-  emoji.innerHTML = "☾";
+  disableDarkMode();
 } else {
+  enableDarkMode();
+}
+initParticles();
+
+function enableDarkMode() {
   document.body.classList.remove("dark-mode");
   emoji.innerHTML = "☀";
 }
-initParticles();
+
+function disableDarkMode() {
+  document.body.classList.add("dark-mode");
+  emoji.innerHTML = "☾";
+}
